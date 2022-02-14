8e99958bd5bc793bfc4183ad8b7c3d4d144f2429
Ian <IanSapp128@gmail.com>
2022-02-14 15:39:21 -0600

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,452 @@
+'''
+TagTool - 2022
+author: SappI
+'''
+import os
+import pandas as pd
+from PyQt5 import QtWidgets, QtGui, QtCore
+import sys
+import pickle
+import platform
+
+# Import UI files
+from ui.mainWindow import Ui_MainWindow
+from ui.editor import Ui_editorWindow
+from ui.diag import Ui_aboutWindow
+from ui.newItem import Ui_newItemWindow
+
+# Update this with each release
+versionNumber = 1.0
+appPath = os.path.dirname(__file__)
+platName = platform.system()
+
+# Window classes
+class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(mainWindow, self).__init__()
+        self.setupUi(self)        
+        self.act_quit.triggered.connect(quitApp)
+        self.act_open.triggered.connect(openFile)
+        self.act_clearTable.triggered.connect(clearTable)
+        self.act_about.triggered.connect(showAbout)
+        self.act_editor.triggered.connect(showEditor)
+        self.btnSetColumn.clicked.connect(setCol)
+        self.btnTagAll.clicked.connect(tagAll)
+        self.btnTagEach.clicked.connect(tagEach)
+        self.btn_save.clicked.connect(saveFile)
+        self.menu_openFile.triggered.connect(openFile)
+        self.menu_keywordEditor.triggered.connect(showEditor)
+        self.menu_clearTable.triggered.connect(clearTable)
+
+class editorWindow(QtWidgets.QMainWindow, Ui_editorWindow):
+    def __init__(self):
+        super(editorWindow, self).__init__()
+        self.setupUi(self)
+        
+        self.act_close.triggered.connect(closeEditor)
+        self.act_save.triggered.connect(saveDefault)
+        self.btn_newEntry.clicked.connect(newEntry)
+        self.btn_deleteEntry.clicked.connect(deleteEntry)
+        self.btn_save.clicked.connect(saveAndClose)
+        self.tblEdit.cellChanged.connect(cellTest)
+
+class aboutWindow(QtWidgets.QMainWindow, Ui_aboutWindow):
+    def __init__(self):
+        super(aboutWindow, self).__init__()
+        self.setupUi(self)
+        self.btnOk.clicked.connect(closeAbout)
+        self.setWindowFlag(QtCore.Qt.WindowMinimizeButtonHint, False)
+        self.lblVersion.setText(f'Version {versionNumber}')
+
+class newItem(QtWidgets.QMainWindow, Ui_newItemWindow):
+    def __init__(self):
+        super(newItem, self).__init__()
+        self.setupUi(self)
+        self.btnCreate.clicked.connect(createNewItem)
+        self.btnCancel.clicked.connect(closeNewItem)
+
+# Globals
+df = pd.DataFrame
+dfEditor = pd.DataFrame
+dfNew = pd.DataFrame
+fileLoaded = False
+activeColumn = 0
+editorLoaded = False
+
+# Data from res.dat will be stored in this dictionary
+iconDict = {}
+
+# Initialize the app
+app = QtWidgets.QApplication([])
+
+# Create a DF
+def createDF(fName):
+    df = pd.read_csv(fName)
+    return df
+
+def readDat(datFile):
+    # Reads resource data packed in the res.dat file
+    global iconDict
+    f=open(datFile, 'rb')
+    iconDict = pickle.load(f)
+    f.close()
+
+def showAbout():
+    uiDiag.show()
+
+# Show error message
+def errorMessage(text, title):
+    msg = QtWidgets.QMessageBox()
+    msg.setIcon(QtWidgets.QMessageBox.Critical)
+    msg.setText(text)
+    msg.setWindowTitle(title)
+    msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    retval = msg.exec()
+
+def showEditor():
+    uiEditor.show()
+    populateEditor()
+
+def showNewItem():
+    uiNewItem.txtEntryName.clear()
+    uiNewItem.show()
+
+def closeNewItem():
+    uiNewItem.close()
+
+# Change the color of rows depending on column
+# Needs RGB values passed
+def colorRows(table, column, r, g, b):
+    col = column
+    for row in range(table.rowCount()):
+        table.item(row, col).setBackground(QtGui.QColor(r,g,b))
+
+
+def createNewItem():
+    # Add new item to the DF and then update the editor table with updated DF
+    global dfEditor
+    categoryName = uiNewItem.txtEntryName.text()
+    if len(categoryName.strip(' ')) < 1:
+        errorMessage("Category cannot be empty", "Error")
+        uiNewItem.txtEntryName.clear()
+    else:
+        newRow = {'Category':f'{categoryName}', 'Keywords':"", 'Blacklist':""}
+        dfEditor = dfEditor.append(newRow, ignore_index=True)
+        uiNewItem.close()
+        write_dt_to_Editor(dfEditor, editorTable)
+
+def saveAndClose():
+    global dfEditor
+    try:
+        if platName == 'Darwin':
+            dfEditor.to_json(f'{appPath}/default.json', orient='records')
+        else:
+            dfEditor.to_json('default.json', orient='records')
+        uiEditor.close()
+    except:
+        errorMessage("Error saving default.json\nYou may not have sufficient write access to the default directory.")
+
+def saveDefault():
+    global df
+    if platName == 'Darwin':
+        dfEditor.to_json(f'{appPath}/default.json', orient='records')
+    else:
+        dfEditor.to_json('default.json', orient='records')
+    setEditorStatus("Default config saved")
+
+def setEditorStatus(text):
+    editorStatusBar.showMessage(text)
+
+def quitApp():
+    sys.exit()
+
+def openFile():
+    global table
+    global df
+    global fileLoaded
+    try:
+        filename = QtWidgets.QFileDialog.getOpenFileName(None, 'Select Dir', os.getcwd(), "CSV Files(*.csv)")[0]
+    except:
+        print("No file found")
+    if filename:
+        try:
+            df = createDF(filename)
+            write_dt_to_qTable(df, table)
+            fileLoaded = True
+            ui.btnSetColumn.setEnabled(True)
+        except:
+            errorMessage("Error opening file", "Error")
+    else:
+        print("Empty filename")
+
+def tagAll():
+    global df
+    global dfNew
+    global activeColumn
+    global progressBar
+
+    saveButton.setEnabled(False)
+    dfNew = df.copy()
+    dfNew['Topic Categories'] = ''
+
+    # Convert all data to string. Function crashes if its another datatype
+    dfNew = dfNew.astype(str)
+
+    if platName == 'Darwin':
+        dfJson = pd.read_json(f'{appPath}/default.json')
+    else:
+        dfJson = pd.read_json("default.json")
+
+    check = False
+    commentList = []
+
+    # Loop through each table item and add the text to the
+    # commentList list
+    for x in dfNew.index:
+        commentList.append(dfNew.iloc[x,activeColumn])
+
+    # Set progress bar to the length of the commentList
+    progressBar.setMaximum(len(commentList))
+
+    for stringIndex, item in enumerate(commentList):
+        string = item
+        check = False
+        for index, row in dfJson.iterrows():
+            blacklistWords = row['Blacklist'].split(",")
+            keywords = row['Keywords'].split(",")
+            category = row['Category']
+            check = False
+
+            # Loop through the blacklist and attempt to find a match.
+            # If match, delete that word before we perform a search
+            for word in blacklistWords:
+                if len(word) > 0:
+                    if word in string.lower():
+                        string.replace(item, '')
+
+            # Loop through the keywords and look for a match
+            # if a match is found, we can enable the CHECK
+            # variable which prevents it from counting the word
+            # twice.
+            for word in keywords:                
+                if len(word) > 0:
+                    if word in string.lower() and check == False:
+                        if len(dfNew['Topic Categories'].iloc[stringIndex]) < 1:
+                            dfNew['Topic Categories'].iloc[stringIndex] = category
+                        else:
+                            dfNew['Topic Categories'].iloc[stringIndex] = dfNew['Topic Categories'].iloc[stringIndex] + f', {category}'
+                        check = True
+        # Update the progress bar and process the changes
+        # so the UI updates.
+        progressBar.setValue(stringIndex)
+        app.processEvents()
+    progressBar.setValue(0)
+    write_dt_to_qTable(dfNew, table)
+    colorRows(table, activeColumn, 129, 189, 147)
+    saveButton.setEnabled(True)
+
+def tagEach():
+    global df
+    global dfNew
+    global activeColumn
+    global progressBar
+
+    saveButton.setEnabled(False)
+    dfNew = df.copy()
+
+    dfNew = dfNew.astype(str)
+
+    if platName == 'Darwin':
+        dfJson = pd.read_json(f'{appPath}/default.json')
+    else:
+        dfJson = pd.read_json("default.json")
+    check = False
+    commentList = []
+    
+    for index, row in dfJson.iterrows():
+        category = row['Category']
+        dfNew[category] = ''
+
+    for x in dfNew.index:
+        commentList.append(dfNew.iloc[x,activeColumn])
+    
+    progressBar.setMaximum(len(commentList))
+        
+    for stringIndex, item in enumerate(commentList):
+        string = item
+        check = False
+        for index, row in dfJson.iterrows():
+            blacklistWords = row['Blacklist'].split(",")
+            keywords = row['Keywords'].split(",")
+            category = row['Category']           
+            check = False
+            for word in blacklistWords:
+                if len(word) > 0:
+                    if word in string.lower():
+                        string.replace(item, '')
+
+            for word in keywords:                
+                if len(word) > 0:
+                    #print(word)
+                    if word in string.lower() and check == False:                     
+                        dfNew[category].iloc[stringIndex] = 'Y'
+                        #bustedCount += 1
+                        check = True
+        progressBar.setValue(stringIndex)
+        app.processEvents()
+    progressBar.setValue(0)
+    write_dt_to_qTable(dfNew, table)
+    saveButton.setEnabled(True)
+
+def clearTable():
+    global df
+    global table
+    global activeColumn
+    saveButton.setEnabled(False)
+    df = pd.DataFrame(None)
+    write_dt_to_qTable(df, table)
+    ui.btnTagAll.setEnabled(False)
+    ui.btnTagEach.setEnabled(False)
+    ui.btnSetColumn.setEnabled(False)
+    ui.txtActiveStat.setText('No Active Column selected')
+    activeColumn = 0
+
+def setCol():
+    global table
+    global activeColumn
+    colorRows(table, activeColumn, 255, 255, 255)
+    activeColumn = table.currentColumn()
+    colorRows(table, activeColumn, 129, 189, 147)
+    table.clearSelection()
+    ui.txtActiveStat.setText(f'The current active column is {activeColumn + 1}')
+    ui.btnTagAll.setEnabled(True)
+    ui.btnTagEach.setEnabled(True)
+    print(activeColumn)
+
+def closeEditor():
+    uiEditor.close()
+
+def populateEditor():
+    global dfEditor
+    global editorLoaded
+    editorLoaded = False
+    try:
+        if platName == 'Darwin':
+            dfEditor = pd.read_json(f'{appPath}/default.json')
+        else:
+            dfEditor = pd.read_json("default.json")
+
+        write_dt_to_Editor(dfEditor, editorTable)
+        editorLoaded = True
+    except:
+        errorMessage("default.json not found!", "Error")
+        closeEditor()
+
+def write_dt_to_qTable(df, table):
+    headers = list(df)
+    table.setRowCount(df.shape[0])
+    table.setColumnCount(df.shape[1])
+    table.setHorizontalHeaderLabels(headers)
+    df_array = df.values
+    for row in range(df.shape[0]):
+        for col in range(df.shape[1]):
+            table.setItem(row, col, QtWidgets.QTableWidgetItem(str(df_array[row,col])))
+
+def write_dt_to_Editor(df, editorTable):
+        global editorLoaded
+        editorLoaded = False
+        headers = list(df)
+        editorTable.setRowCount(df.shape[0])
+        editorTable.setColumnCount(df.shape[1])
+        editorTable.setHorizontalHeaderLabels(headers)
+        df_array = df.values
+        for row in range(df.shape[0]):
+            for col in range(df.shape[1]):
+                editorTable.setItem(row, col, QtWidgets.QTableWidgetItem(str(df_array[row,col])))
+        editorLoaded = True
+
+def newEntry():
+    showNewItem()
+
+def deleteEntry():
+    global dfEditor
+    index = editorTable.currentRow()
+    print(index)
+    if index != -1:
+        dfEditor = dfEditor.drop(index, axis=0)
+        dfEditor.reset_index(drop=True, inplace=True)
+        write_dt_to_Editor(dfEditor, editorTable)
+    else:
+        pass
+
+def cellTest():
+    global dfEditor
+    global editorLoaded
+    if editorLoaded:
+        print("cell changed")
+        col = uiEditor.tblEdit.currentColumn()
+        row = uiEditor.tblEdit.currentRow()
+        tableItem = uiEditor.tblEdit.item(row, col).text()
+        print(tableItem)
+        dfEditor.iloc[row, col] = tableItem.replace(', ', ',')
+        write_dt_to_Editor(dfEditor, editorTable)
+        editorStatusBar.showMessage(str(tableItem))
+
+def saveFile():
+    global dfNew
+    try:
+        filename = QtWidgets.QFileDialog.getSaveFileName(None, 'Save File', os.getcwd(), "CSV Files(*.csv)")[0]
+        print(filename)
+        dfNew.to_csv(str(filename), index=False)
+    except:
+        pass
+
+def iconFromBase64(base64):
+    pixmap = QtGui.QPixmap()
+    pixmap.loadFromData(QtCore.QByteArray.fromBase64(base64))
+    icon = QtGui.QIcon(pixmap)
+    return icon
+
+def imageFromBase64(base64):
+    pixmap = QtGui.QPixmap()
+    pixmap.loadFromData(QtCore.QByteArray.fromBase64(base64))
+    return pixmap
+
+def closeAbout():
+    uiDiag.close()
+
+def loadData():
+    try:
+        if platName == 'Darwin':
+            readDat(f'{appPath}/res.dat')
+        else:
+            readDat('res.dat')
+        
+        app.setWindowIcon(iconFromBase64(bytes(iconDict['icon.png'], encoding='utf8')))
+        ui.menu_openFile.setIcon(iconFromBase64(bytes(iconDict['folder.png'], encoding='utf8')))
+        ui.menu_keywordEditor.setIcon(iconFromBase64(bytes(iconDict['keyword.png'], encoding='utf8')))
+        ui.menu_clearTable.setIcon(iconFromBase64(bytes(iconDict['clear.png'], encoding='utf8')))
+        uiDiag.pixLogo.setPixmap(imageFromBase64(bytes(iconDict['logo.png'], encoding='utf8')))
+    except:
+        errorMessage('Could not load res.dat\nApplication will still run without graphical icons and logos.', 'Error')
+        uiDiag.pixLogo.setText("TagTool")
+
+# Set up the window objects        
+ui = mainWindow()
+uiEditor = editorWindow()
+uiDiag = aboutWindow()
+uiNewItem = newItem()
+
+# Variables
+table = ui.tableWidget
+progressBar = ui.progressBar
+saveButton = ui.btn_save
+editorTable = uiEditor.tblEdit
+editorStatusBar = uiEditor.statusbar
+
+# Load in the image data from res.dat
+loadData()
+
+# Show main window and exec the program
+ui.show()
+app.exec()
